5be936354bfae8146e2a6f6d4803d769f0786dac
rwiggins1 <wigginsricky94@gmail.com>
2025-06-14 14:13:34 -0500

    Add Binary search Tree base

---
+++ PyForgeDS/base/BSTNode.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+class BSTNode:
+    def __init__(info: Any):
+        self.info = info
+        self.left = None
+        self.right = None
---
+++ PyForgeDS/trees/BinarySearchTree.py
@@ -0,0 +1,15 @@
+from typing import Any
+from PyForgeDS.base.BSTNode import BSTNode
+
+class BinarySearchTree:
+    def __init__():
+        root = None
+
+    def isEmpty():
+        raise NotImplementedError()
+
+    def min():
+        raise NotImplementedError()
+
+    def max():
+        raise NotImplementedError()
