f274a2c5726092ff9b2bb3cc41e33496504dbb60
rwiggins1 <wigginsricky94@gmail.com>
2025-09-13 17:22:01 -0500

    test(LinkBasedList): add assertions

--- tests/LBList_test.py
+++ tests/LBList_test.py
@@ -5,8 +5,10 @@ def test_add():
     assert lbl.num_elements == 0
     lbl.add(0, 10)
     lbl.add(1, 20)
-    print(lbl.front.data, lbl.rear.data)
+    assert lbl.getFront().getData() == 10
+    assert lbl.getRear().getData() == 20
     lbl.add(0, 1)
-    print(lbl.front.data, lbl.front.next.data, lbl.rear.data)
-    print(lbl)
+    assert lbl.getFront().getData() == 1
+    assert lbl.getFront().getNext().getData() == 10
+    assert lbl.getRear().getData() == 20
 
