56fbc0459c9282219eafd35430c596a01762c727
rwiggins1 <wigginsricky94@gmail.com>
2025-09-13 23:54:47 -0500

    refactor: use getter and setter methods for BST

--- PyForgeDS/trees/BinarySearchTree.py
+++ PyForgeDS/trees/BinarySearchTree.py
@@ -17,58 +17,58 @@ class BinarySearchTree:
             return False
         
         if target < node.info:
-            return self.recContains(target, node.left)
+            return self.recContains(target, node.getLeft())
         if target > node.info:
-            return self.recContains(target, node.right)
+            return self.recContains(target, node.getRight())
         
         return True
 
     def contains(self, target: Any) -> bool:
-        return self.recContains(target, self.root)
+        return self.recContains(target, self.getHead())
 
     def recGet(self, target: Any, node: BSTNode) -> Any:
         if node == None:
             return None
         
         if target < node.info:
-            return self.recGet(target, node.left)
+            return self.recGet(target, node.getInfo())
         if target > node.info:
-            return self.recGet(target, node.right)
+            return self.recGet(target, node.getRight())
         
         return node.info
 
     def get(self, target: Any) -> Any:
-        return self.recGet(target, self.root)
+        return self.recGet(target, self.getHead())
     
     def min(self) -> Any:
-        node = self.root
+        node = self.getHead()
 
         if self.isEmpty():
             return None
         else:
-            while node.left:
-                node = node.left
-            return node.info
+            while node.getLeft():
+                node = node.getLeft()
+            return node.getInfo()
 
     def max(self) -> Any:
-        node = self.root
+        node = self.getHead()
         
         if self.isEmpty():
             return None
         else:
-            while node.right:
-                node = node.right
-            return node.info
+            while node.getRight():
+                node = node.getRight()
+            return node.getInfo()
 
     def recAdd(self, info: Any, node: BSTNode) -> None:
-        if info < node.info:
-            if node.left:
-                return self.recAdd(info, node.left)
+        if info < node.getInfo():
+            if node.getLeft():
+                return self.recAdd(info, node.getLeft())
             else:
                 node.left = BSTNode(info)
-        if info > node.info:
-            if node.right:
-                return self.recAdd(info, node.right)
+        if info > node.getInfo():
+            if node.getRight():
+                return self.recAdd(info, node.getRight())
             else:
                 node.right = BSTNode(info)
 
@@ -76,41 +76,41 @@ class BinarySearchTree:
         if self.isEmpty():
             self.root = BSTNode(info)
         else:
-            self.recAdd(info, self.root)
+            self.recAdd(info, self.getHead())
         self.node_num+=1
 
     def recRemove(self, target: Any, node: BSTNode) -> BSTNode:
         if node is None:
             return None
 
-        if target < node.info:
-            node.left = self.recRemove(target, node.left)
-        elif target > node.info:
-            node.right = self.recRemove(target, node.right)
+        if target < node.getInfo():
+            node.left = self.recRemove(target, node.getLeft())
+        elif target > node.getInfo():
+            node.right = self.recRemove(target, node.getRight())
         else:
             # Node with one or no child
             if node.left is None:
-                return node.right
+                return node.getRight()
             elif node.right is None:
-                return node.left
+                return node.getLeft()
 
             # Node with two children
             # Find in-order successor (smallest in right subtree)
-            min_larger_node = self._minNode(node.right)
-            node.info = min_larger_node.info
-            node.right = self.recRemove(min_larger_node.info, node.right)
+            min_larger_node = self._minNode(node.getRight())
+            node.setInfo(min_larger_node)
+            node.setRight(self.recRemove(min_larger_node.getInfo(), node.getRight()))
 
         return node
 
     def _minNode(self, node: BSTNode) -> BSTNode:
-        while node.left:
-            node = node.left
+        while node.getLeft():
+            node = node.getLeft()
         return node
 
     def remove(self, target: Any) -> bool:
         if not self.contains(target):
             return False
-        self.root = self.recRemove(target, self.root)
+        self.root = self.recRemove(target, self.getHead())
         self.node_num-=1
         return True
 
