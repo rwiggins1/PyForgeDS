6b811d17ce9bea35039b9c3e388e216ea0bbc022
rwiggins1 <wigginsricky94@gmail.com>
2025-09-13 23:10:13 -0500

    feat: add getters to BSTNode

--- PyForgeDS/base/BSTNode.py
+++ PyForgeDS/base/BSTNode.py
@@ -5,6 +5,15 @@ class BSTNode:
         self.info = info
         self.left = None
         self.right = None
+
+    def getInfo(self) -> Any:
+        return self.info
+    
+    def getLeft(self) -> Any:
+        return self.left
+    
+    def getRight(self) -> Any:
+        return self.right
         
     def __repr__(self):
             return f"BSTNode({self.info}, left={self.left}, right={self.right})"
